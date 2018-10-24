7f8d474f67649d773e6ebc1b27486c869b67cf53
Adam <zulfurix@gmail.com>
2018-10-24 20:42:11 +0100

    Screen Fading Method

--- GTAOnlineClient/ScreenEvents.cs
+++ GTAOnlineClient/ScreenEvents.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using CitizenFX.Core.UI;
 
 namespace GTAOnlineClient
 {
-    class ScreenEvents
+    class ScreenEvents : BaseScript
     {
+        public ScreenEvents()
+        {
+            EventHandlers.Add("ScreenFade", new Action<int, int>(ScreenFade));
+        }
+
+        public void ScreenFade(int time, int fadeType) //fadeTypes = [0] For fading in, [1] For fading out
+        {
+            switch (fadeType)
+            {
+                case 0:
+                    Screen.Fading.FadeIn(time);
+                    break;
+                case 1:
+                    Screen.Fading.FadeOut(time);
+                    break;
+            }
+        }
     }
 }
