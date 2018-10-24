f93b3cfd36919b7011711058798951a8019ef2a2
Adam <zulfurix@gmail.com>
2018-10-24 20:47:39 +0100

    Screen events in wrong project

---
+++ GTAOnlineServer/ScreenEvents.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using CitizenFX.Core.UI;
+
+namespace GTAOnlineShared
+{
+    class ScreenEvents : BaseScript
+    {
+        public ScreenEvents()
+        {
+            EventHandlers.Add("ScreenFade", new Action<int, int>(ScreenFade));
+        }
+
+    }
+}
