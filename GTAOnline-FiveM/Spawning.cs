9794cbd1d29063406c1807d7be4a9ca27f869149
Adam <zulfurix@gmail.com>
2018-10-27 15:00:55 +0100

    Work on basic spawn system

---
+++ GTAOnline-FiveM/Spawning.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using CitizenFX.Core.UI;
+
+namespace GTAOnline_FiveM
+{
+    public class Spawning : BaseScript
+    {
+        public Spawning()
+        {
+            EventHandlers.Add("GTAO:AlertPlayerJoined", new Action<String>(AlertPlayerJoined));
+            EventHandlers.Add("GTAO:AlertPlayerLeft", new Action<String>(AlertPlayerLeft));
+        }
+
+        private void AlertPlayerJoined(String playerName)
+        {
+            Screen.ShowNotification("~h~" + playerName + " ~s~joined.");
+        }
+
+        private void AlertPlayerLeft(String playerName)
+        {
+            Screen.ShowNotification("~h~" + playerName + " ~s~left.");
+        }
+    }
+}
---
+++ GTAOnline-Fivem-Server/Spawning.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+
+namespace GTAOnline_Fivem_Server
+{
+    public class Spawning : BaseScript
+    {
+        public static string motd = "Welcome to Adam's FiveM server!";
+
+        public Spawning()
+        {
+            Tick += OnTick;
+            EventHandlers.Add("playerConnecting", new Action<Player>(OnPlayerSpawned));
+            EventHandler.Add("playerDropped", new Action<Player>(OnPlayerDropped));
+        }
+
+        private async Task OnTick()
+        {
+            await Delay(0);
+        }
+
+        private void OnPlayerSpawned([FromSource] Player source)
+        {
+            Debug.WriteLine(source.Name + " has connected to the server from " + source.EndPoint);
+            TriggerClientEvent(source, "chatMessage", new[] { 255, 0, 0 }, motd);
+            TriggerClientEvent("GTAO:AlertPlayerJoined", source.Name);
+        }
+
+        private void OnPlayerDropped([FromSource] Player source)
+        {
+            Debug.WriteLine(source.Name + " has left the server");
+            TriggerClientEvent(source, "chatMessage", new[] { 255, 0, 0 }, motd);
+            TriggerClientEvent("GTAO:AlertPlayerLeft", source.Name);
+        }
+    }
+}
