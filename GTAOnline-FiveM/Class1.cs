5d6616a598bb2c2164dbefd1ab1c21fdc554fec8
Adam <zulfurix@gmail.com>
2018-10-27 14:40:51 +0100

    Add project files

---
+++ GTAOnline-FiveM/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTAOnline_FiveM
+{
+    public class Class1
+    {
+    }
+}
