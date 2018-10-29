2d65292d46b094fb5aced9ac9dfcdcc64d9da37e
Adam <zulfurix@gmail.com>
2018-10-29 21:12:42 +0000

    Converted Mirror Park Party Script

---
+++ GTAOnline-FiveM/MirrorParkParty.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using CitizenFX.Core.UI;
+
+namespace GTAOnline_FiveM
+{
+    class MirrorParkParty : BaseScript
+    {
+        int iLocal_0 = 0;
+
+        public MirrorParkParty()
+        {
+            Tick += OnTick;
+        }
+
+        private async Task OnTick()
+        {
+            if (NetworkIsHost())
+            {
+                if (HasForceCleanupOccurred(18))
+                {
+                    func_1();
+                }
+                await Delay(0);
+                if (!IsWorldPointWithinBrainActivationRange())
+                {
+                    iLocal_0 = 3;
+                }
+
+                switch (iLocal_0)
+                {
+                    case 0:
+                        if (GetClockHours() >= 22 || GetClockHours() <= 4)
+                        {
+                            iLocal_0 = 1;
+                        }
+                        else
+                        {
+                            func_1();
+                        }
+                        break;
+                    case 1:
+                        if (!IsIplActive("ID2_21_G_Night"))
+                        {
+                            RequestIpl("ID2_21_G_Night");
+                            iLocal_0 = 2;
+                        }
+                        break;
+                    case 2: break;
+                    case 3:
+                        if (!IsNewLoadSceneActive() && !IsPlayerSwitchInProgress())
+                        {
+                            func_1();
+                        }
+                        break;
+                }
+            }
+        }
+
+        private void func_1()
+        {
+            if (IsIplActive("ID2_21_G_Night"))
+            {
+                RemoveIpl("ID2_21_G_Night");
+            }
+        }
+    }
+}
