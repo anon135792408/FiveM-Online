28d77cc7f634131b9d6d3311aea9cb765f8282aa
Adam <zulfurix@gmail.com>
2018-11-22 19:49:55 +0000

    Work to do

---
+++ GTAOnline-FiveM/VehicleProperties.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using CitizenFX.Core.UI;
+
+namespace GTAOnline_FiveM
+{
+    class VehicleProperties : BaseScript
+    {
+        public VehicleProperties()
+        {
+            EventHandlers.Add("GTAO:clientToggleVehicleEngine", new Action(TogglePlayerEngine));
+        }
+
+        private void TogglePlayerEngine()
+        {
+            if (Game.PlayerPed.CurrentVehicle != null && Game.PlayerPed.CurrentVehicle.Driver == Game.PlayerPed)
+            {
+                Game.PlayerPed.CurrentVehicle.IsEngineRunning = !Game.PlayerPed.CurrentVehicle.IsEngineRunning;
+            }
+        }
+    }
+}
---
+++ GTAOnline-Fivem-Server/Commands.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+
+namespace GTAOnline_Fivem_Server
+{
+    class Commands : BaseScript
+    {
+        public Commands()
+        {
+            API.RegisterCommand("engine", new Action<Player, List<object>, string>((source, arguments, raw) =>
+            {
+                TriggerClientEvent(source.Handle, "GTAO:clientToggleVehicleEngine");
+            }), false);
+        }
+    }
+}
