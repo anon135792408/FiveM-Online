36397a5f67d991479d052ed794542b65ac9e7b7d
Adam <zulfurix@gmail.com>
2018-11-23 08:37:26 +0000

    Register engine command in server and create VehicleProperties class

---
+++ GTAOnline-FiveM/VehicleProperties.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using CitizenFX.Core.UI;
+
+namespace GTAOnline_FiveM
+{
+    class VehicleProperties : BaseScript
+    {
+
+    }
+}
