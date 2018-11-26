ce7ffe03ab26ac44f45804886c20dd4d2d4cc5ec
Adam <zulfurix@gmail.com>
2018-11-26 11:49:43 +0000

    UserHandler class

---
+++ GTAOnline-FiveM/UserHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using CitizenFX.Core.UI;
+
+namespace GTAOnline_FiveM {
+    class UserHandler : BaseScript {
+        Player localPlayer;
+
+        public UserHandler() {
+            EventHandlers.Add("playerSpawned", new Action(OnPlayerSpawned));
+        }
+
+        public void OnPlayerSpawned() {
+
+        }
+    }
+}
