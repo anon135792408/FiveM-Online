c2bc6c7adec41e32b6c4dead1fc9ca7aaa993441
Adam <zulfurix@gmail.com>
2018-11-26 11:34:47 +0000

    Added Player and PersonalVehicle class

---
+++ GTAOnline-FiveM/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using CitizenFX.Core.UI;
+
+namespace GTAOnline_FiveM {
+    class Player {
+        private string name;
+        private long cash;
+        private List<PersonalVehicle> personalVehicles;
+
+        public Player() {
+            name = "NoName";
+            cash = 25000;
+            personalVehicles = new List<PersonalVehicle>();
+        }
+
+        public string Name { get => name; set => name = value; }
+        public long Cash { get => cash; set => cash = value; }
+        public List<PersonalVehicle> PersonalVehicles { get => personalVehicles; set => personalVehicles = value; }
+    }
+}
