4fb7578181a8db77d0f6aac93380a30adfb14146
Rees Morris <reesmorriscontact@gmail.com>
2018-12-14 19:35:33 +0000

    CharacterCreation base code

---
+++ source/GTAOnline-FiveM/CharacterCreation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using CitizenFX.Core.UI;
+using NativeUI;
+
+namespace GTAOnline_FiveM {
+    class CharacterCreation : BaseScript {
+        Vector3 coordinates = new Vector3(408f, -998f, -99f);
+    }
+}
