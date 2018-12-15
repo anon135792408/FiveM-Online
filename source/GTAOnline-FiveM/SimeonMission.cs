6dd54d7ee541dbed56eab3a30426945ae8aa8d67
Adam <zulfurix@gmail.com>
2018-12-15 15:05:20 +0000

    Various minor improvements to Simeon Missions

--- source/GTAOnline-FiveM/SimeonMission.cs
+++ source/GTAOnline-FiveM/SimeonMission.cs
@@ -30,23 +30,35 @@ namespace GTAOnline_FiveM {
 
             if (!missionActive && NetworkIsHost()) {
                 Tuple<Vector3, float> randPos = GetRandomPosition();
+                Debug.WriteLine("A");
                 RequestCollisionAtCoord(randPos.Item1.X, randPos.Item1.Y, randPos.Item1.Z);
+                Debug.WriteLine("B");
                 missionVehicle = await World.CreateVehicle(GetRandomVehHash(), randPos.Item1, randPos.Item2);
+                Debug.WriteLine("C");
                 missionVehicle.PlaceOnGround();
+                Debug.WriteLine("D");
                 missionVehicle.LockStatus = VehicleLockStatus.CanBeBrokenInto;
+                Debug.WriteLine("E");
                 missionVehicle.IsAlarmSet = true;
+                Debug.WriteLine("F");
                 missionVehicle.IsPersistent = true;
-                
+                Debug.WriteLine("G");
+
 
                 SetVehicleHasBeenOwnedByPlayer(missionVehicle.Handle, true);
+                Debug.WriteLine("H");
                 var net_id = NetworkGetNetworkIdFromEntity(missionVehicle.Handle);
+                Debug.WriteLine("I");
                 SetNetworkIdCanMigrate(net_id, true);
+                Debug.WriteLine("J");
                 SetNetworkIdExistsOnAllMachines(net_id, true);
-
+                Debug.WriteLine("K");
+                NetworkFadeOutEntity(missionVehicle.Handle, true, false);
                 await Delay(5000);
                 TriggerServerEvent("GTAO:StartMissionForAll", net_id);
                 TriggerServerEvent("GTAO:DisplaySimeonMarkerForAll");
                 missionActive = true;
+                NetworkFadeInEntity(missionVehicle.Handle, true);
             }
         }
 
@@ -99,6 +111,9 @@ namespace GTAOnline_FiveM {
         }
 
         private void EndMission() {
+            if (NetworkIsHost() && missionVehicle != null) {
+                NetworkFadeOutEntity(missionVehicle.Handle, true, false);
+            }
             missionActive = false;
             missionVehicle.AttachedBlip.Delete();
             missionVehicle.MarkAsNoLongerNeeded();
