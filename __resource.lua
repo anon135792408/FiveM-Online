636ba56824ad625f41d116120ad2ea5a478a4bb7
Adam <zulfurix@gmail.com>
2019-01-01 22:05:38 +0000

    Update file names in __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -3,10 +3,10 @@ resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
 christmas_enabled 'false'
 
 client_script {
-	"GTAOnline-FiveM.net.dll",
+	"FiveM-Online-Client.net.dll",
 	"NativeUI.dll"
 }
 
 server_script {
-	"GTAOnline-Fivem-Server.net.dll",
+	"FiveM-Online-Server.net.dll",
 }
