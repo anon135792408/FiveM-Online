2ab0cacf35b405cee17f76214b179e3a2400b478
Adam <zulfurix@gmail.com>
2019-01-19 13:37:35 +0000

    Create apartments.cs

---
+++ source/GTAOnline-FiveM/Apartments.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using CitizenFX.Core.UI;
+using NativeUI;
+
+namespace FiveM_Online_Client
+{
+    class Apartments : BaseScript
+    {
+
+    }
+}
