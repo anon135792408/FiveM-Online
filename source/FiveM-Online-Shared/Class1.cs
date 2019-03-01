e8a8ff74d39610ebab28fc0c6e1db66ef34f65b9
Zulfurix <zulfurix@gmail.com>
2019-03-01 14:49:38 +0000

    Add shared project

---
+++ source/FiveM-Online-Shared/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiveM_Online_Shared
+{
+    public class Class1
+    {
+    }
+}
